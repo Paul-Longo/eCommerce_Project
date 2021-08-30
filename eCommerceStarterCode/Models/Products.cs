4f0a69c1373556232bb4c2df2241fb94913f5afe
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-30 15:13:25 -0500

    updated erd

---
+++ eCommerceStarterCode/Models/Products.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public ICollection<ShoppingCart> ShoppingCarts { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Category> Categories { get; set; }
