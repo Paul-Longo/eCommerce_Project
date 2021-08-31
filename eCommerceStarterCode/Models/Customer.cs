425b5a1324712891bac52c42e7fb59fca6688309
spencerjamesdev <spencer.franke@gmail.com>
2021-08-31 11:04:05 -0500

    Added ShoppingCart Controller

--- eCommerceStarterCode/Models/Customer.cs
+++ eCommerceStarterCode/Models/Customer.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace eCommerceStarterCode.Models
 {
-    public class Customer
+    public class Customer : IdentityUser
     {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+
     }
 }
--- eCommerceStarterCode/Models/ShoppingCart.cs
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace eCommerceStarterCode.Models
 {
     public class ShoppingCart
     {
+        public int ShoppingCartId { get; set; }
+        public string Product { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
