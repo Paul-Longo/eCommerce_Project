4370e8a55325614fda7e790a3e713297debe4345
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-31 10:47:40 -0500

    productcontroller

---
+++ eCommerceStarterCode/Controllers/ProductControllers.cs
@@ -0,0 +1,61 @@
+using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Start requests here.
+        // get all merch 
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var product = _context.Products;
+            return Ok(product);
+        }
+        // get merch by id TESTED
+        [HttpGet("{id}")]
+        public IActionResult GetMerchById(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        // post or "add" new merch to database TESTED
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+        // update merch by id TESTED
+
+        // delete merch by id TESTED 
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+    }
+}
