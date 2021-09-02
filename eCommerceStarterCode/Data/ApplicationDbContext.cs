8d97034c89d0cbcdad89f12fba76349c86200252
Paul-Longo <imanazarite88@gmail.com>
2021-09-02 11:33:00 -0400

    resolved merge conflicts

--- eCommerceStarterCode/Data/ApplicationDbContext.cs
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -8,19 +8,24 @@ namespace eCommerceStarterCode.Data
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions options)
-            :base(options)
+            : base(options)
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductOrder> ProductOrders { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+        public DbSet<User> UserId { get; set; }
+     
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfiguration(new RolesConfiguration());
         }
-
     }
 }
